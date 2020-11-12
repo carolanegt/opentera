d9cf77292d608cdf760470f95c80ecd7a7955a87
cvilanova <cyvila@gmail.com>
2020-11-12 11:11:42 -0500

    get reservations between dates

--- teraserver/python/services/RoomReservation/libroomreservation/db/DBManagerReservationAccess.py
+++ teraserver/python/services/RoomReservation/libroomreservation/db/DBManagerReservationAccess.py
@@ -6,5 +6,17 @@ import calendar
 class DBManagerReservationAccess:
 
     def query_reservation_by_id(self, reservation_id: int):
-        room = RoomReservationReservation.get_reservation_by_id(reservation_id)
-        return room
+        reservation = RoomReservationReservation.get_reservation_by_id(reservation_id)
+        return reservation
+
+    def query_reservation_by_room(self, room_id, start_date, end_date):
+        start_date = datetime.datetime.strptime(start_date, '%d-%m-%Y').date()
+        end_date = datetime.datetime.strptime(end_date, '%d-%m-%Y').date()
+
+        reservations = RoomReservationReservation.query.filter(
+            RoomReservationReservation.reservation_start_datetime.between(start_date, end_date),
+            RoomReservationReservation.id_room == room_id).all()
+
+        if reservations:
+            return reservations
+        return []
