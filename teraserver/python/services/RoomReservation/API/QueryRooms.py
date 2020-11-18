64ea9366034fd2f413342b2d79d0610785c35f8d
cvilanova <cyvila@gmail.com>
2020-11-18 16:16:06 -0500

    Update QueryRooms.py

--- teraserver/python/services/RoomReservation/API/QueryRooms.py
+++ teraserver/python/services/RoomReservation/API/QueryRooms.py
@@ -1,6 +1,5 @@
-from flask import jsonify, session, request
+from flask import jsonify, request
 from flask_restx import Resource, reqparse, inputs
-from modules.LoginModule.LoginModule import user_multi_auth
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy import exc
 from flask_babel import gettext
@@ -17,7 +16,10 @@ get_parser.add_argument('id_site', type=int, help='ID of the site from which to 
 get_parser.add_argument('reservations', type=inputs.boolean, help='Flag that expands the returned data to include '
                                                                   'reservations')
 
-post_parser = api.parser()
+post_schema = api.schema_model('room', {'properties': RoomReservationRoom.get_json_schema(),
+                                        'type': 'object',
+                                        'location': 'json'})
+
 delete_parser = reqparse.RequestParser()
 delete_parser.add_argument('id', type=int, help='Room ID to delete', required=True)
 
@@ -32,7 +34,7 @@ class QueryRooms(Resource):
     @api.doc(description='Get rooms information. Only one of the ID parameter is supported and required at once',
              responses={200: 'Success - returns list of rooms',
                         500: 'Database error'})
-    # @AccessManager.token_required
+    @AccessManager.token_required
     def get(self):
         parser = get_parser
 
@@ -61,8 +63,7 @@ class QueryRooms(Resource):
                                          'get', 500, 'InvalidRequestError', str(e))
             return gettext('Invalid request'), 500
 
-    @user_multi_auth.login_required
-    @api.expect(post_parser)
+    @api.expect(post_schema)
     @api.doc(description='Create / update rooms. id_room must be set to "0" to create a new '
                          'room. A room can be created/modified if the user has admin rights to the '
                          'related site.',
@@ -70,6 +71,7 @@ class QueryRooms(Resource):
                         403: 'Logged user can\'t create/update the specified room',
                         400: 'Badly formed JSON or missing fields(id_site) in the JSON body',
                         500: 'Internal error occurred when saving room'})
+    @AccessManager.token_required
     def post(self):
         room_access = DBManager.roomAccess()
         # Using request.json instead of parser, since parser messes up the json!
@@ -79,7 +81,10 @@ class QueryRooms(Resource):
         if 'id_room' not in room_json or 'id_site' not in room_json:
             return gettext('Missing id_room or id_site arguments'), 400
 
-        # TODO Only site admins can create new rooms
+        if 'room_reservations' in room_json:
+            room_reservations = room_json.pop('room_reservations')
+
+        # TODO Only site admins can create or update rooms
         # if room_json['id_room'] == 0 and room_json['id_site'] not in room_access.get_accessible_sites_ids(
         #        admin_only=True):
         #    return gettext('Forbidden'), 403
@@ -117,12 +122,12 @@ class QueryRooms(Resource):
 
         return jsonify([update_room.to_json()])
 
-    @user_multi_auth.login_required
     @api.expect(delete_parser)
     @api.doc(description='Delete a specific room',
              responses={200: 'Success',
                         403: 'Logged user can\'t delete room (only site admin can delete)',
                         500: 'Database error.'})
+    @AccessManager.token_required
     def delete(self):
         parser = delete_parser
         # current_user = TeraUser.get_user_by_uuid(session['_user_id'])
@@ -133,10 +138,8 @@ class QueryRooms(Resource):
 
         # Check if current user can delete
         # TODO Only site admins can delete a room
-        room = RoomReservationRoom.get_room_by_id(id_todel)
-
-        if room_access.get_site_role(room.room_site.id_site) != 'admin':
-            return gettext('Forbidden'), 403
+        # if room_access.get_site_role(room.room_site.id_site) != 'admin':
+        #   return gettext('Forbidden'), 403
 
         # If we are here, we are allowed to delete. Do so.
         try:
