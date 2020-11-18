1c48273e1c426807ee0d9a7870316730454e8bc1
cvilanova <cyvila@gmail.com>
2020-11-18 16:16:24 -0500

    Update RoomReservationReservation.py

--- teraserver/python/services/RoomReservation/libroomreservation/db/models/RoomReservationReservation.py
+++ teraserver/python/services/RoomReservation/libroomreservation/db/models/RoomReservationReservation.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import timedelta
 
 from services.RoomReservation.libroomreservation.db.Base import db, BaseModel
 
@@ -6,7 +7,7 @@ from services.RoomReservation.libroomreservation.db.Base import db, BaseModel
 class RoomReservationReservation(db.Model, BaseModel):
     __tablename__ = "t_reservations"
     id_reservation = db.Column(db.Integer, db.Sequence('id_reservation_sequence'), primary_key=True, autoincrement=True)
-    id_room = db.Column(db.Integer, db.ForeignKey('t_rooms'), nullable=False)
+    id_room = db.Column(db.Integer, db.ForeignKey('t_rooms'), nullable=True)
     session_uuid = db.Column(db.String(36), nullable=True, unique=True)
     user_uuid = db.Column(db.String(36), nullable=False)
     reservation_start_datetime = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
@@ -40,4 +41,44 @@ class RoomReservationReservation(db.Model, BaseModel):
         base_reservation.user_uuid = '7234fd5c-7486-4206-910e-02aa43282f1e'
         base_reservation.reservation_duration = 0.5
         base_reservation.reservation_start_datetime = datetime.datetime.now()
+        RoomReservationReservation.insert(base_reservation)
+
+        base_reservation = RoomReservationReservation()
+        base_reservation.id_room = 1
+        base_reservation.user_name = 'admin'
+        base_reservation.user_uuid = '7234fd5c-7486-4206-910e-02aa43282f1e'
+        base_reservation.reservation_duration = 2
+        base_reservation.reservation_start_datetime = datetime.datetime.now() + timedelta(days=1)
+        RoomReservationReservation.insert(base_reservation)
+
+        base_reservation = RoomReservationReservation()
+        base_reservation.id_room = 1
+        base_reservation.user_name = 'admin'
+        base_reservation.user_uuid = '7234fd5c-7486-4206-910e-02aa43282f1e'
+        base_reservation.reservation_duration = 2
+        base_reservation.reservation_start_datetime = datetime.datetime.now() + timedelta(days=1) + timedelta(hours=2)
+        RoomReservationReservation.insert(base_reservation)
+
+        base_reservation = RoomReservationReservation()
+        base_reservation.id_room = 1
+        base_reservation.user_name = 'admin'
+        base_reservation.user_uuid = '7234fd5c-7486-4206-910e-02aa43282f1e'
+        base_reservation.reservation_duration = 1.5
+        base_reservation.reservation_start_datetime = datetime.datetime.now() + timedelta(days=6)
+        RoomReservationReservation.insert(base_reservation)
+
+        base_reservation = RoomReservationReservation()
+        base_reservation.id_room = 1
+        base_reservation.user_name = 'admin'
+        base_reservation.user_uuid = '7234fd5c-7486-4206-910e-02aa43282f1e'
+        base_reservation.reservation_duration = 1.25
+        base_reservation.reservation_start_datetime = datetime.datetime.now() + timedelta(days=2)
+        RoomReservationReservation.insert(base_reservation)
+
+        base_reservation = RoomReservationReservation()
+        base_reservation.id_room = 1
+        base_reservation.user_name = 'admin'
+        base_reservation.user_uuid = '7234fd5c-7486-4206-910e-02aa43282f1e'
+        base_reservation.reservation_duration = 1
+        base_reservation.reservation_start_datetime = datetime.datetime.now() + timedelta(hours=2)
         RoomReservationReservation.insert(base_reservation)
