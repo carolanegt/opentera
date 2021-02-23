90bcaa3980962cd9a1f6073ece52a4dbc5d8e84e
cvilanova <cyvila@gmail.com>
2021-02-23 10:23:08 -0500

    get user site role

--- teraserver/python/services/RoomReservation/API/QueryUserInfos.py
+++ teraserver/python/services/RoomReservation/API/QueryUserInfos.py
@@ -5,6 +5,12 @@ from opentera.services.ServiceAccessManager import ServiceAccessManager, current
     LoginType
 from services.BureauActif.FlaskModule import default_api_ns as api
 
+# Parser definition(s)
+from services.RoomReservation import Globals
+
+get_parser = api.parser()
+get_parser.add_argument('id_site', type=int, help='ID of the selected site')
+
 
 class QueryUserInfos(Resource):
 
@@ -12,10 +18,13 @@ class QueryUserInfos(Resource):
         Resource.__init__(self, _api, *args, **kwargs)
         self.module = kwargs.get('flaskModule', None)
 
+    @api.expect(get_parser)
     @api.doc(description='Gets user infos from token: user_name, user_uuid',
              responses={200: 'Success'})
     @ServiceAccessManager.token_required
     def get(self):
+        parser = get_parser
+        args = parser.parse_args()
         user_infos = {
             'user_uuid': 'unknown',
             'user_fullname': 'unknown',
@@ -26,5 +35,18 @@ class QueryUserInfos(Resource):
             user_infos['user_uuid'] = current_user_client.user_uuid
             user_infos['user_fullname'] = current_user_client.user_fullname
             user_infos['is_super_admin'] = current_user_client.user_superadmin
+
+        # If reservation has a session associated to it, get it from OpenTera
+        if args['id_site']:
+            params = {'id_site': args['id_site'], 'uuid_user': current_user_client.user_uuid}
+
+            endpoint = '/api/service/users/access'
+            response = Globals.service.get_from_opentera(endpoint, params)
+
+            if response.status_code == 200:
+                role = response.json()
+                if args['id_site']:
+                    site_admin = True if role['site_role'] == 'admin' else False
+                    user_infos.update({'is_site_admin': site_admin})
 
         return user_infos
